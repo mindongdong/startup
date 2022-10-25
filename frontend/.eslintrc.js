adafbbe428af1883e5648f6eb08139d5e39ec183
mindong <74493250+mindongdong@users.noreply.github.com>
2022-10-25 14:59:32 +0900

    frontend project setting
    frontend project setting
    
    vue-cli create
    eslint, prettier setting
    components, views, router create

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": [
+      "error",
+      // 아래 규칙들은 개인 선호에 따라 prettier 문법 적용
+      // https://prettier.io/docs/en/options.html
+      {
+        singleQuote: false,
+        semi: true,
+        useTabs: false,
+        tabWidth: 2,
+        trailingComma: "all",
+        printWidth: 80,
+        bracketSpacing: false,
+        arrowParens: "avoid",
+      },
+    ],
+    "vue/multi-word-component-names": "off",
+  },
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <router-link to="/analysis">video</router-link>
+    <router-link to="/main">main</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
