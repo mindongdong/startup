e563399be7e24905cae970398c1e4b818736c05d
mindong <74493250+mindongdong@users.noreply.github.com>
2023-03-10 00:12:05 +0900

    update capstone

--- backend/app/routes/endpoints/matches.py
+++ backend/app/routes/endpoints/matches.py
@@ -15,16 +15,16 @@ def checkInfo(match_info: matchInfo):
     teams = match_info.match_name.split('.')
     team1 = teams[0]
     team2 = teams[1]
-    print(team1, team2)
+    # print(team1, team2)
     PATH_MATCH_INFO = f'./matches/{match_info.match_name}/'
     match_list = os.listdir(PATH_MATCH_INFO)
     match_data = []
-    print(PATH_MATCH_INFO + match_list[0])
+    # print(PATH_MATCH_INFO + match_list[0])
     for data in match_list:
-        print(data)
+        # print(data)
         with open(PATH_MATCH_INFO + data, 'r') as file:
             file_data = pd.read_csv(file).fillna('').to_dict().copy()
-            print(file_data)
+            # print(file_data)
             match_data.append(file_data) 
 
     return JSONResponse(match_data)
